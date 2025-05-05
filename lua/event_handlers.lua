edf7cd8c4e4d5e35cc5bcd27620bc191ad3fb5d6
risethesummer <honhatlinhmail@gmail.com>
2025-05-05 14:19:59 +0700

    remove theme changer

--- lua/event_handlers.lua
+++ lua/event_handlers.lua
@@ -6,30 +6,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
       vim.highlight.on_yank()
     end,
 })
-
--- Theme changer for Neovim in VSCode
-vim.api.nvim_exec([[
-    " THEME CHANGER
-    function! SetCursorLineNrColorInsert(mode)
-        " Insert mode: blue
-        if a:mode == "i"
-            call VSCodeNotify('nvim-theme.insert')
-
-        " Replace mode: red
-        elseif a:mode == "r"
-            call VSCodeNotify('nvim-theme.replace')
-        endif
-    endfunction
-
-    augroup CursorLineNrColorSwap
-        autocmd!
-        autocmd ModeChanged *:[vV\x16]* call VSCodeNotify('nvim-theme.visual')
-        autocmd ModeChanged *:[R]* call VSCodeNotify('nvim-theme.replace')
-        autocmd InsertEnter * call SetCursorLineNrColorInsert(v:insertmode)
-        autocmd InsertLeave * call VSCodeNotify('nvim-theme.normal')
-        autocmd CursorHold * call VSCodeNotify('nvim-theme.normal')
-        autocmd ModeChanged [vV\x16]*:* call VSCodeNotify('nvim-theme.normal')
-    augroup END
-]], false)
-
-
